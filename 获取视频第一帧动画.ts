6a63ccb537e4208513c6891444c61d4a7e393625
linmingqi <2320447654@qq.com>
2023-12-11 13:57:26 +0800

    获取视频第一帧动画

---
+++ 获取视频第一帧动画.ts
@@ -0,0 +1,72 @@
+/**
+ * Get the first frame of a video as a data URL.
+ * @param videoUrl - 视频url链接.
+ * @param options - 额外字段.
+ * @param options.width - 导出图片宽度.
+ * @param options.height - 导出图片高度.
+ * @param options.num - 选择视频第几帧动画.
+ * @returns base64图片
+ */
+export const getFirstFrame = (
+  videoUrl: string,
+  { width = 300, height = 150, num = 1 }: options = {}
+): Promise<string> => {
+  const video = document.createElement('video');
+  video.width = 640;
+  video.height = 360;
+  video.controls = true;
+  video.src = videoUrl;
+	video.crossOrigin = 'Anonymous'
+	video.currentTime = num
+
+  Object.assign(video.style, {
+    position: 'fixed',
+    zIndex: '-999',
+    opacity: '0',
+  });
+
+  document.body.appendChild(video);
+
+  return new Promise((resolve, reject) => {
+    video.addEventListener('loadeddata', async () => {
+      const canvas = document.createElement('canvas');
+      document.body.appendChild(canvas);
+
+      Object.assign(canvas.style, {
+        position: 'fixed',
+        zIndex: '-999',
+        opacity: '0',
+      });
+
+      const ctx = canvas.getContext('2d');
+      if (!ctx) {
+        reject(new Error('Canvas 2D context is not supported.'));
+        return;
+      }
+
+      ctx.drawImage(video, 0, 0, width, height);
+      const dataURL = canvas.toDataURL();
+      resolve(dataURL);
+
+      setTimeout(() => {
+        document.body.removeChild(canvas);
+        document.body.removeChild(video);
+      }, 100);
+    });
+
+    video.addEventListener('error', (error) => {
+      reject(error);
+
+      setTimeout(() => {
+        document.body.appendChild(video);
+      }, 100);
+    });
+  });
+};
+
+
+type options = {
+  width?: number;
+  height?: number;
+	num?: number
+};
